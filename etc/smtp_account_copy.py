19afb8dcc9f3b95b6a28b3753db1f118a67cf95d
zzyyshi <zzyyshi@qq.com>
2018-05-08 23:35:07 +0800

    +mail

---
+++ etc/smtp_account_copy.py
@@ -0,0 +1,18 @@
+# smtp_account 的去敏
+SMTP_ACCOUNT = [
+    {
+        "name": "XXXXXXXXX@gmail.com",  # 邮箱账号
+        "pwd": "XXXX",  # 邮箱密码
+        "smtp": "smtp.gmail.com",  # smtp服务器地址
+    },
+    {
+        "name": "17X@qq.com",  # 邮箱账号
+        "pwd": "XXXXXXXX",  # 邮箱密码
+        "smtp": "smtp.qq.com",  # smtp服务器地址
+    },
+    {
+        "name": "XXXXXXX@sina.com",  # 邮箱账号
+        "pwd": "XXXXXXXXXX",  # 邮箱密码
+        "smtp": "smtp.sina.com",  # smtp服务器地址
+    }
+]
--- spider.py
+++ spider.py
@@ -12,6 +12,8 @@ from bs4 import BeautifulSoup
 import smtplib
 import requests
 from pickleshare import PickleShareDB
+from utils.header import getHeader
+import etc.smtp_account as smtp_accounts
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -36,27 +38,22 @@ logger = logging.getLogger(__name__)
 class Spider:
     def get(self):
         url = "https://www.cdfangxie.com/Infor/type/typeid/36.html"
-        html = requests.get(url, verify=False)
+        html = requests.get(url, verify=False, headers=getHeader())
         bs = BeautifulSoup(html.text, "html.parser")
         tables = bs.find(class_='right_cont')
         tables_a = tables.find_all("a")
         db = PickleShareDB('data')
         history_list = db.get("history_list", [])
+        smtpObj = self.get_smtp()
         for row in tables_a:
             try:
                 title = row["title"]
                 url = "https://www.cdfangxie.com" + row["href"]
-                # area = title.split("|")[0]
-                # name = title.split("|")[1]
-                # dic = {
-                #     "area": title.split("|")[0],
-                #     "name": title.split("|")[1]
-                # }
                 if title in history_list:
                     logger.info("Crawl:%s  result:pass", title)
                 else:
                     logger.info("Crawl:%s  result:send_email", title)
-                    if self.send_email(title, url):
+                    if self.send_email(smtpObj, title, url):
                         logger.info("send_email success")
                         history_list.append(title)
                     else:
@@ -64,29 +61,48 @@ class Spider:
             except:
                 pass
         db["history_list"] = history_list
+        smtpObj.quit()
 
-    def send_email(self, title, url):
+    def send_email(self, smtpObj, title, url):
         sender = 'pywechat@sina.com'
         receivers = ['zzyyshi@qq.com', 'pywechat@sina.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
         # 第三方 SMTP 服务
-        mail_host = "smtp.sina.com"  # 设置服务器
-        mail_user = "pywechat@sina.com"  # 用户名
-        mail_pass = "zywechat"  # 口令
         message = MIMEText("地址:{} \r\n\r\n\r\nFrom Joy's Python".format(url), 'plain', 'utf-8')
         message['From'] = Header(sender)
         message['To'] = Header(receivers[0], 'utf-8')
         message['Subject'] = Header(title, 'utf-8')
         try:
-            smtpObj = smtplib.SMTP()
-            smtpObj.connect(mail_host, 25)
-            smtpObj.login(mail_user, mail_pass)
-            smtpObj.sendmail(sender, receivers, message.as_string())
+            ans = smtpObj.sendmail(sender, receivers, message.as_string())
+            logger.info(str(ans))
             return True
-        except smtplib.SMTPException as e:
+        except Exception as e:
             logger.error(str(e))
             return False
 
+    def get_smtp(self, debug=False):
+        for accounts in smtp_accounts.SMTP_ACCOUNT:
+            logger.info("=============================================")
+            logger.info("连接服务器 %s", accounts["smtp"])
+            mail_host = accounts["smtp"]  # 设置服务器
+            mail_user = accounts["name"]  # 用户名
+            mail_pass = accounts["pwd"]  # 口令
+            smtpObj = smtplib.SMTP_SSL(mail_host)
+            try:
+                ans = smtpObj.login(mail_user, mail_pass)
+                if ans[0] == 235 or ans[0] == 503:
+                    logger.info("登录成功")
+                else:
+                    logger.info("登录失败:%s %s", ans[0],
+                                str(ans[1]))  # 235 == 'Authentication successful' 503 == 'Error: already authenticated'
+                if debug:
+                    logger.info("测试成功：%s", mail_user)
+                else:
+                    return smtpObj
+            except Exception as e:
+                logger.info("测试服务器失败%s", str(e))
+        return False
+
 
 if __name__ == "__main__":
-    # Spider().get()
-    Spider().send_email("x", "https://www.cdfangxie.com/Infor/index/id/4201.html")
+    # Spider().get_smtp(debug=True)
+    Spider().get()
